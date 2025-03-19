2c0c7d9dcdf0d4031538c0cd578372285f1a9cf0
Federico Tocci <federico.tocci99@libero.it>
2025-03-19 23:03:25 +0100

    commit per test deployment

--- src/MealGenius.js
+++ src/MealGenius.js
@@ -6,7 +6,7 @@ function MealGenius() {
   return (
     <div style={{ color: "white" }}>
       <h1>Interfaccia Meal Genius</h1>
-      <p>Il tuo assistente per i pasti</p>
+      <p>Il tuo assistente per i pasti.</p>
       <p></p>
     </div>
   );
